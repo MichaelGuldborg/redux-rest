9e6ae89407cf4fdbf3cd4b63ca24d337fba53520
Michael Guldborg <mail@michaelguldborg.dk>
2020-11-23 12:37:20 +0100

    Remove src/hooks/index

--- src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./useCrudRestState";
-export * from "./useCurrentElement";
-export * from "./useReduxCrudState";
-export * from "./useRequestList";
-export * from "./useSocketCrudState";
-export * from "./useSubmitButtonRef";
