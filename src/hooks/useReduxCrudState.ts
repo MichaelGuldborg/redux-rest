6ac52454eeb12cdb909a0076abaafc656b834ef7
Michael Guldborg <mail@michaelguldborg.dk>
2020-11-23 12:41:20 +0100

    Update useReduxCrudState

--- src/hooks/useReduxCrudState.ts
+++ src/hooks/useReduxCrudState.ts
@@ -1,10 +1,10 @@
 import {CrudRestState, CrudValueRestActionType} from "../store";
-import {DefaultRootState, useDispatch, useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import Identifiable from "../models/Identifyable";
 
 
-export const useReduxCrudState = <T extends Partial<Identifiable>, TState = DefaultRootState>(selector: (state: TState) => CrudRestState<T>, actions: CrudValueRestActionType<T>, parentId: string) => {
+export const useReduxCrudState = <TState, T extends Partial<Identifiable>>(selector: (state: TState) => CrudRestState<T>, actions: CrudValueRestActionType<T>, parentId: string) => {
     const dispatch = useDispatch();
     const state = useSelector(selector);
     const elementsLength = state.elements?.length;
